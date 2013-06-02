9f596648fd8a4e6674ab5073e0e1787b7246d712
archblob <fcsernik@gmail.com>
2013-06-03 00:55:04 +0300

    Rename Problem 2 to RNA.

--- RNA/RNA.hs
+++ RNA/RNA.hs
@@ -1,6 +1,6 @@
-module RNATranscription (
-    transcribeDNAtoRNA
-    ) where
+module RNA (
+   transcribeDNAtoRNA
+ ) where
 
 -- [1] Simple implementation
 
@@ -10,8 +10,8 @@ type RNAString = String
 transcribeNuc:: Char -> Char
 transcribeNuc n =
     case n of
-        'T' -> 'U'
-        _   ->  n
+      'T' -> 'U'
+      _   ->  n
 
 transcribeDNAtoRNA:: DNAString -> RNAString
 transcribeDNAtoRNA = map transcribeNuc
