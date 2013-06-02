d33c03db226eaf9877ae30b0d64e532fd2ad70bf
archblob <fcsernik@gmail.com>
2013-06-03 00:41:46 +0300

    Clean Problem DNA.

--- Problem.1.Counting_Nucleotides/CountingNucleotides.hs
+++ Problem.1.Counting_Nucleotides/CountingNucleotides.hs
@@ -6,17 +6,17 @@ module CountingNucleotides (
 -- [1] Simple implementation
 type NucleotideFreq = (Int,Int,Int,Int)
 
-neutralFreq::NucleotideFreq
-neutralFreq = (0,0,0,0)
+unitFreq::NucleotideFreq
+unitFreq = (0,0,0,0)
 
 countNucleotide:: NucleotideFreq -> Char -> NucleotideFreq
 countNucleotide (a,c,g,t) n =
     case n of
-        'A' -> (a+1,c,g,t)
-        'C' -> (a,c+1,g,t)
-        'G' -> (a,c,g+1,t)
-        'T' -> (a,c,g,t+1)
-        _   -> (a,c,g,t)
+      'A' -> (a+1,c,g,t)
+      'C' -> (a,c+1,g,t)
+      'G' -> (a,c,g+1,t)
+      'T' -> (a,c,g,t+1)
+      _   -> (a,c,g,t)
 
-computeNucFreq:: [Char] -> NucleotideFreq
-computeNucFreq = foldl countNucleotide neutralFreq
+computeNucFreq:: String -> NucleotideFreq
+computeNucFreq = foldl countNucleotide unitFreq
